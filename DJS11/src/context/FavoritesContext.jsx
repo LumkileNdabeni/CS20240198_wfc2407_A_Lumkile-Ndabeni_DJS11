1d6bb92521b41763042acf7ade3f885866d7a2cf
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-11-27 00:47:35 +0200

    Completed favourites function

--- DJS11/src/context/FavoritesContext.jsx
+++ DJS11/src/context/FavoritesContext.jsx
@@ -3,7 +3,19 @@ import React, { createContext, useState } from 'react';
 export const FavoritesContext = createContext();
 
 export const FavoritesProvider = ({ children }) => {
-    const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
-    const addFavorite = (show) => {
-        setFavorites((prev) => [...prev, show]);
+  const addFavorite = (show) => {
+    setFavorites((prev) => [...prev, show]);
+  };
+
+  const removeFavorite = (id) => {
+    setFavorites((prev) => prev.filter((show) => show.id !== id));
+  };
+
+  return (
+    <FavoritesContext.Provider value={{ favorites, addFavorite, removeFavorite }}>
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
