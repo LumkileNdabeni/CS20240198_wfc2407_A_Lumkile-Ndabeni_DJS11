490520ec4033b17b59591b8051a1a693dcab4530
Lumkile Ndabeni <ndabenilumkile@gmail.com>
2024-11-27 10:23:37 +0200

    Diplayed ShowCard in a grid

--- DJS11/src/App.jsx
+++ DJS11/src/App.jsx
@@ -7,19 +7,19 @@ import { FavoritesProvider } from './context/FavoritesContext';
 import './styles/App.css';
 
 const App = () => {
-    return (
-        <FavoritesProvider>
-            <Router>
-                <div className="app">
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/show/:id" element={<ShowDetail />} />
-                        <Route path="/favorites" element={<Favorites />} />
-                    </Routes>
-                </div>
-            </Router>
-        </FavoritesProvider>
-    );
+  return (
+    <FavoritesProvider>
+      <Router>
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Home />} /> {/* Correct route path */}
+            <Route path="/show/:id" element={<ShowDetail />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+      </Router>
+    </FavoritesProvider>
+  );
 };
 
 export default App;
--- DJS11/src/components/ShowCard.jsx
+++ DJS11/src/components/ShowCard.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ShowCard = ({ show }) => {
-    return (
-        <div className="show-card">
-            <img src={show.image} alt={show.title} />
-            <h3>{show.title}</h3>
-            <p>{show.description}</p>
-        </div>
-    );
+  const imageStyle = {
+    width: "200px", // Adjust the width as needed
+    height: "auto", // Maintain aspect ratio
+  };
+
+  return (
+    <div className="show-card">
+      <img src={show.image} alt={show.title} style={imageStyle} />
+      <Link to={`/show/${show.id}`}>
+        <h3>{show.title}</h3>
+      </Link>
+      <p>{show.description}</p>
+    </div>
+  );
 };
 
 export default ShowCard;
